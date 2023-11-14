09124774062e926a100d29478abc6379e3dde702
kode <duedumaxwell43@gmail.com>
2023-11-14 05:40:05 -0800

    prototype of print_%.c

--- main.h
+++ main.h
@@ -6,5 +6,6 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int printCharacters(va_list, charac);
 int printStrings(va_list, str);
-
+int _strlen(char *s);
+int printPercent(void);
 #endif /* MAIN_H*/
