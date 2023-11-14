9d5ea4ac00ace18f149741b534fa416d33ea5298
kode <duedumaxwell43@gmail.com>
2023-11-14 05:15:47 -0800

    function for prints_s.c

--- print_s.c
+++ print_s.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _printStrings - prints a string followed by a new line
+ * printStrings - prints a string followed by a new line
  * @str: pointer to a string
  */
 
-void _printStrings(va_list, str)
+void printStrings(va_list, str)
 {
 	char *string
 	int i = 0;
