7d4c3037a8900dbacf8bd10726102161aeef8310
kode <duedumaxwell43@gmail.com>
2023-11-14 05:27:08 -0800

    function for strlen

---
+++ print_strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - return the length the length of a string
+ * @s: string to be calculated
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
